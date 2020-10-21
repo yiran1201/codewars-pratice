7497d88157c73bfc5531953f59bf77167f5f024f
yiran1201 <yiranchen1201@gmail.com>
2020-10-20 23:21:30 -0700

    update

--- 知识点/example.js
+++ 知识点/example.js
@@ -1,13 +1,34 @@
+function tickets(peopleInLine){
+  var deposit = [0,0,0];
 
-/**
- * 1. Last in first out
- * 2. push, pop
- */
-const stack = [];
+  function updateDeposit(paid) {
+    for(var i=0; i<deposit.length; i++) {
+      deposit[i] = deposit[i] + paid[i];
+    }
+  }
 
+  for (var index in peopleInLine) {
 
-/**
- * 1. First in first out
- * 2. push, shift
- */
-const queue = []
+    if(peopleInLine[index] === 25) {
+      updateDeposit([1,0,0])
+    }
+
+    else if(peopleInLine[index] === 50) {
+      updateDeposit([-1,1,0])
+    }
+
+    else {// pay 100
+      if (deposit[0] >=1 && deposit[1] >=1){
+          updateDeposit([-1,-1,1]);}
+      else if (deposit[0] >=3 && deposit[1] == 0){
+          updateDeposit([-3, 0, 1]);}
+      else {
+          updateDeposit([-3, 0, 1]);}
+    }
+
+    if (deposit[0] < 0 || deposit[1] < 0 || deposit[2] < 0 ) {
+      return 'NO';
+    }
+  }
+  return 'YES';
+}
